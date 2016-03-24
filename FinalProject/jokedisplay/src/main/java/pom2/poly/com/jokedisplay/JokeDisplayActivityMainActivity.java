ee7dce7d43bb752b824750f8279e7ba4785b595d
roy989898 <roy989898@gmail.com>
2016-03-24 14:21:30 +0800

    set the dependency
    Joke telling>backend>app
    joke display>app

--- FinalProject/jokedisplay/src/main/java/pom2/poly/com/jokedisplay/JokeDisplayActivityMainActivity.java
+++ FinalProject/jokedisplay/src/main/java/pom2/poly/com/jokedisplay/JokeDisplayActivityMainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.example.JokeTeller;
-
 public class JokeDisplayActivityMainActivity extends AppCompatActivity {
 
     private TextView tvJoke;
@@ -15,7 +13,7 @@ public class JokeDisplayActivityMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joke_display_activity_main);
         tvJoke = (TextView) findViewById(R.id.tvJoke);
-        String joke = new JokeTeller().getJoke();
-        tvJoke.setText(joke);
+//        String joke = new JokeTeller().getJoke();
+//        tvJoke.setText(joke);
     }
 }
